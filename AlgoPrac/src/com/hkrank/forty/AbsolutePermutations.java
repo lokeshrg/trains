cae23b790e9b6f03827ebd71fa14c568f7e4fe09
Lokesh Reddy <9510488+lokeshrg@users.noreply.github.com>
2017-10-09 23:07:02 -0400

    Algorithms Practise

---
+++ AlgoPrac/src/com/hkrank/forty/AbsolutePermutations.java
@@ -0,0 +1,13 @@
+package com.hkrank.forty;
+
+/** 
+ * https://www.hackerrank.com/challenges/absolute-permutation
+ * */
+public class AbsolutePermutations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AlgoPrac/src/com/hkrank/thirty/StrangeCounter.java
@@ -0,0 +1,36 @@
+package com.hkrank.thirty;
+
+import java.util.Scanner;
+
+public class StrangeCounter {
+	public static void main(String[] args) {
+		// 3, 6, 12, 24
+		// 1, 2, 4, 8
+		// 3i.2^j
+		// i from 1 to 10, j from 0 to 5
+		// given t, find counter
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		s.close();
+		for (int i = 0, counter = 1; counter <= t; i++) {
+			for (long j = (long) (3 * Math.pow(2, i)); j > 0; --j) {
+				System.out.print(counter + ":" + j + "; ");
+				counter++;	
+				if (counter == t) {
+					System.out.println("\nCounter for " + t + " is :" + j);
+					return;
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static long betterSolution(long t){
+		long rem = 3;
+		while(t < rem){
+			t -= rem;
+			rem *= 2;
+		}
+		return rem-t+1;
+	}
+}
